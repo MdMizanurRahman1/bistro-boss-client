9cfa133b248d9504f22dd95458e29112acb8defc
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-06-04 17:03:00 +0300

    delete and all mange options done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -15,11 +15,10 @@ const Dashboard = () => {
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content">
                 {/* <*-- Page content here --*> */}
-                <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+                <Outlet></Outlet>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
@@ -32,7 +31,7 @@ const Dashboard = () => {
                             <>
                                 <li><NavLink to='/dashboard/home'><FaHome></FaHome> ADMIN HOME</NavLink></li>
                                 <li><NavLink to='/dashboard/addItem'><FaUtensils></FaUtensils> ADD AN ITEM</NavLink></li>
-                                <li><NavLink to='/dashboard/history'><FaWallet></FaWallet> MANAGE ITEMS</NavLink></li>
+                                <li><NavLink to='/dashboard/manageitems'><FaWallet></FaWallet> MANAGE ITEMS</NavLink></li>
                                 <li><NavLink to='/dashboard/history'><FaBook></FaBook> MANAGE BOOKING</NavLink></li>
                                 <li><NavLink to='/dashboard/allusers'><FaUsers></FaUsers> ALL USERS</NavLink></li>
 
--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -2,13 +2,52 @@ import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { FaUtensils } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
+
+
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 
 const AddItem = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [axiosSecure] = useAxiosSecure();
+    const { register, handleSubmit, reset } = useForm();
+
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
+
     const onSubmit = data => {
-        console.log(data)
+        const formData = new FormData();
+        formData.append('image', data.image[0])
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    const { name, price, category, recipe } = data;
+                    const newItem = { name, price: parseFloat(price), category, recipe, image: imgURL }
+                    console.log(newItem);
+                    axiosSecure.post('/menu', newItem)
+                        .then(data => {
+                            console.log('after posting new menu item', data.data)
+                            if (data.data.insertedId) {
+                                reset();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Item added successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
+                }
+            })
     }
-    console.log(errors);
+
+
     return (
         <div className='w-full px-14 py-10'>
             <SectionTitle subHeading="What's new" heading="Add an item"></SectionTitle>
@@ -25,8 +64,8 @@ const AddItem = () => {
                         <label className="label">
                             <span className="label-text">Category*</span>
                         </label>
-                        <select {...register("category", { required: true })} className="select select-bordered">
-                            <option disabled selected>Category</option>
+                        <select defaultValue="Category" {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled>Category</option>
                             <option>Pizza</option>
                             <option>Soup</option>
                             <option>Salad</option>
@@ -45,13 +84,13 @@ const AddItem = () => {
                     <label className="label">
                         <span className="label-text">Recipe Details*</span>
                     </label>
-                    <textarea {...register("details", { required: true })} className="textarea textarea-bordered h-24 w-[660px]" placeholder="Recipe Details"></textarea>
+                    <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24 w-[660px]" placeholder="Recipe Details"></textarea>
                 </div>
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
                         <span className="label-text">Item Image*</span>
                     </label>
-                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full max-w-xs" />
                 </div>
                 <div className='flex items-center btn btn-sm w-28 gap-2 bg-yellow-600 mt-4'>
                     <input type="submit" value="Add Item" />
